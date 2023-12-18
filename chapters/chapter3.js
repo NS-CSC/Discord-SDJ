c4fa4c693f5d7f311fa6be8535a6d93c92bf37a8
mono0218 <mono@monokamo.com>
2023-12-18 15:12:40 +0900

    add: chapter3

---
+++ chapters/chapter3.js
@@ -0,0 +1,24 @@
+import { sdjClient } from "sdj";
+import dotenv from "dotenv";
+const client = new sdjClient();
+dotenv.config();
+
+const message = async message=>{
+    if(message.content === "!omikuzi"){
+        const result = omikuzi();
+        await client.sendMessage(message.channel,`今日の運勢は${result}です！`);
+    }
+}
+
+function omikuzi(){
+    const omikuzi = ["大吉","中吉","小吉","吉","凶","大凶"];
+    return omikuzi[Math.floor(Math.random() * omikuzi.length)];
+}
+
+async function Chapter3() {
+    await client.build(process.env.TOKEN);
+
+    await client.listenOnEvent('messageCreate',message);
+}
+
+Chapter3();
